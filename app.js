eacbb44db2b47612a78b4bbb62cffbef0df8d9b4
al-sw <alanjunior.sweeney@gmail.com>
2024-04-18 11:37:27 +0200

    first commit

---
+++ app.js
@@ -0,0 +1,84 @@
+const taskInput = document.getElementById("taskInput");
+const addBtn = document.getElementById("addBtn");
+const clearBtn = document.getElementById("clearBtn");
+const taskList = document.getElementById("taskList");
+const taskNumber = document.getElementById("taskNumber");
+
+function getTasksFromLocalStorage() {
+  return JSON.parse(localStorage.getItem("tasks")) || [];
+}
+
+function saveTasksToLocalStorage(tasks) {
+  localStorage.setItem("tasks", JSON.stringify(tasks));
+}
+
+function addTask() {
+  const taskName = taskInput.value.trim();
+
+  if (!taskName) {
+    alert("Please enter a valid task name!");
+  } else {
+    const tasks = getTasksFromLocalStorage();
+    tasks.push({ id: Date.now(), name: taskName });
+    saveTasksToLocalStorage(tasks);
+    taskInput.value = "";
+    getTasks();
+  }
+}
+
+function removeTask(id) {
+  const tasks = getTasksFromLocalStorage();
+  const updatedTasks = tasks.filter((task) => task.id !== id);
+  saveTasksToLocalStorage(updatedTasks);
+  getTasks();
+}
+
+function getTasks() {
+  taskList.innerHTML = "";
+
+  const tasks = getTasksFromLocalStorage();
+
+  if (tasks.length === 0) {
+    const message = document.createElement("li");
+    message.textContent = "All tasks completed!";
+    taskList.appendChild(message);
+  } else {
+    tasks.forEach((task) => {
+      const taskItem = document.createElement("li");
+      taskItem.textContent = task.name;
+
+      const deleteButton = document.createElement("button");
+      deleteButton.textContent = "Delete Task";
+      deleteButton.addEventListener("click", () => {
+        removeTask(task.id);
+      });
+
+      taskItem.appendChild(deleteButton);
+      taskList.appendChild(taskItem);
+    });
+  }
+
+  taskNumber.innerText = tasks.length;
+}
+
+function clearList() {
+  localStorage.removeItem("tasks");
+  getTasks();
+}
+
+addBtn.addEventListener("click", () => {
+  addTask();
+});
+
+taskInput.addEventListener("keypress", (e) => {
+  if (e.key === "Enter") {
+    addTask();
+  }
+});
+
+clearBtn.addEventListener("click", () => {
+  clearList();
+  getTasks();
+});
+
+getTasks();
